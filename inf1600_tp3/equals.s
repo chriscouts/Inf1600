d3cdfa614e8e8fd10ef86f959ba3e457c563ab98
Christophe <christophe.couturier@polymtl.ca>
2019-03-09 13:15:06 -0500

    equals.s a debugger

--- inf1600_tp3/equals.s
+++ inf1600_tp3/equals.s
@@ -2,49 +2,46 @@
  
 
 matrix_equals_asm:
-push %ebp
+	push %ebp
 	mov %esp, %ebp 			/* Set ebp to current esp */
-	sub $8, %esp			#alloue 2 octets pour r et c
-	mov $0, %eax
-	mov %eax, -4(%ebp)		#r=0
-	mov %eax, -8(%ebp)		#c=0
-	
-	jmp for
-
+	sub $8, %esp			# alloue 2 octets pour r et c
+	mov $0, %ecx
+	mov %ecx, -4(%ebp)		# r=0
+	mov %ecx, -8(%ebp)		# c=0
+		
 	for:
 		mov -4(%ebp), %eax
-		cmp 16(%ebp), %eax	#for (r < matorder)
-		jnae for2		#verifie le flag
-		leave
-		ret
+		cmp 16(%ebp), %eax	# for (r < matorder)
+		jl for2				# verifie le flag
+		mov $1, %eax		# va return 1
+		jmp return
 
 	for2:
 		mov -8(%ebp), %ebx
-		cmp 16(%ebp), %ebx	#for (c < matorder)
-		jnae if			#verifie le flag
-		inc %ebx		#%ebx++
-		mov %ebx, -8(%ebp)
-		cmp 16(%ebp), %ebx	#for (c < matorder)
-		jnae for2		#recommence la loop
-		mov -4(%ebp), %eax
-		inc %eax		#%eax++
-		mov %eax, -4(%ebp)
-		jmp for
+		cmp 16(%ebp), %ebx	# for (c < matorder)
+		jl if				# verifie le flag
+		add $1, -4(%ebp)
+		jmp for				# recommence le 1er for
 
 	if:
-		imul 16(%ebp), %eax	# matorder * r
-		add %ebx, %eax		# c + matorder * r
-		mov 12(%ebp), %ecx
-		add %eax, %ecx
-		mov 8(%ebp), %edx
-		add %eax, %edx
-		mov (%ecx), %ecx
-		cmp %ecx, (%edx)
-		jne return
-		inc %ebx		#%ebx++
-		mov %ebx, -8(%ebp)
+				mov -4(%ebp), %eax	# %eax = r
+		imul 16(%ebp), %eax # %eax = r * matorder
+		mov -8(%ebp), %edx	# %edx = c
+		add %edx, %eax		# %eax = r * matorder + c
+		mov 8(%ebp), %edx	# %edx = inmatdata1
+		add %eax, %edx		# %edx = inmatdata1[%eax]
+		mov 12(%ebp), %ebx	# %ebx = inmatdata2
+		add %eax, %ebx		# %ebx = inmatdata2[%eax]
+		mov (%ebx), %ebx
+		cmp %ebx, %edx		# cmp inmatdata1[%eax] et inmatdata2[%eax]
+		je incc				# verifie le flag
+		mov $0, %eax
+		jmp return
+
+	incc:
+		add $1, -8(%ebp)
 		jmp for2
 
 	return: 
 		leave          		/* Restore ebp and esp */
-		ret           	/* Return to the caller */
+		ret           		/* Return to the caller */
