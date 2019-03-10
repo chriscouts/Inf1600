40f344ac3011ac66132fb39c0babb3cc92ea36da
Christophe <christophe.couturier@polymtl.ca>
2019-03-10 14:13:03 -0400

    equals.s termine

--- inf1600_tp3/equals.s
+++ inf1600_tp3/equals.s
@@ -5,42 +5,50 @@ matrix_equals_asm:
 	push %ebp
 	mov %esp, %ebp 			/* Set ebp to current esp */
 	sub $8, %esp			# alloue 2 octets pour r et c
-	mov $0, %ecx
-	mov %ecx, -4(%ebp)		# r=0
-	mov %ecx, -8(%ebp)		# c=0
+	
+	movl $0, -4(%ebp)		# r=0
+	
+	jmp for1test
 		
-	for:
+	for1test:
 		mov -4(%ebp), %eax
-		cmp 16(%ebp), %eax	# for (r < matorder)
-		jl for2				# verifie le flag
+		cmp 16(%ebp), %eax  # verifie r < matorder
+		jb for1
 		mov $1, %eax		# va return 1
 		jmp return
 
-	for2:
-		mov -8(%ebp), %ebx
-		cmp 16(%ebp), %ebx	# for (c < matorder)
-		jl if				# verifie le flag
-		add $1, -4(%ebp)
-		jmp for				# recommence le 1er for
+	for1:
+		movl $0, -8(%ebp) 	# c=0
+		jmp for2test
 
-	if:
-				mov -4(%ebp), %eax	# %eax = r
+	for2test:
+		mov -8(%ebp), %ebx
+		cmp 16(%ebp), %ebx	# verifie c < matorder
+		jb	for2
+		incl -4(%ebp)		# ++r
+		jmp for1test
+
+	for2:
+		mov -4(%ebp), %eax	# %eax = r
 		imul 16(%ebp), %eax # %eax = r * matorder
 		mov -8(%ebp), %edx	# %edx = c
 		add %edx, %eax		# %eax = r * matorder + c
-		mov 8(%ebp), %edx	# %edx = inmatdata1
-		add %eax, %edx		# %edx = inmatdata1[%eax]
-		mov 12(%ebp), %ebx	# %ebx = inmatdata2
-		add %eax, %ebx		# %ebx = inmatdata2[%eax]
-		mov (%ebx), %ebx
-		cmp %ebx, %edx		# cmp inmatdata1[%eax] et inmatdata2[%eax]
-		je incc				# verifie le flag
-		mov $0, %eax
-		jmp return
 
-	incc:
-		add $1, -8(%ebp)
-		jmp for2
+		mov 8(%ebp), %edx	# %edx = inmatdata1
+		mov 12(%ebp), %ebx	# %ebx = inmatdata2
+
+		mov 0(%edx, %eax, 4), %edx		# %edx = inmatdata1[%eax]
+		mov 0(%ebx, %eax, 4), %ebx		# %ebx = inmatdata2[%eax]
+
+		cmp %ebx, %edx		# cmp inmatdata1[%eax] et inmatdata2[%eax]
+		je for2end				# verifie le flag
+
+		mov $0, %eax		# va return 0
+		jmp return
+	
+	for2end:
+		incl -8(%ebp)		# ++c
+		jmp for2test
 
 	return: 
 		leave          		/* Restore ebp and esp */
