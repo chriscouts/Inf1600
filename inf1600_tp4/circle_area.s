4f9f49cc12c97b6143548111764cc44b52a4e8ab
Alex <alexandre.touchette@outlook.com>
2019-03-12 09:59:37 -0400

    Modification de circle_area.s

--- inf1600_tp4/circle_area.s
+++ inf1600_tp4/circle_area.s
@@ -4,7 +4,30 @@ _ZNK7CCircle7AreaAsmEv:
         push %ebp      /* save old base pointer */
         mov %esp, %ebp /* set ebp to current esp */
         
-        /* Write your solution here */
+        # Mettre le pointeur this dans %eax
+        mov (this), %eax        # %eax = this
+
+        # Met le mRadius dans %ebx
+        mov 4((this)), %ebx     # %ebx = mRadius
+
+        # Met le mRadius dans %ebx
+        mov 4((this)), %ecx     # %ecx = mRadius
+
+        # Push %ebx au dessus de la pile
+        push %ebx               # st[0] = mRadius
+        
+        # Push %ecx au dessus de la pile
+        push %ecx               # st[0] = st[1] = mRadius
+        
+        # Multiplie st[0] et stp[1]
+        fmulp                   # st[0] = st[1] = mRadius * mRadius
+
+        # Ajoute pi au desus de la pile 
+        fldpi                   # st[0] = pi, st[1] = mRadius * mRadius
+        
+        # Multiplie st[0] et st[1] 
+        fmulp                   # st[0] = st[1] = pi * mRadius * mRadius
+        
         
         leave          /* restore ebp and esp */
         ret            /* return to the caller */
