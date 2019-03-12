880077c681f8a3c45c4963fcfdfb089bbbb7fa9b
Christophe <christophe.couturier@polymtl.ca>
2019-03-12 09:57:32 -0400

    circle perimeter

--- inf1600_tp4/circle_perimeter.s
+++ inf1600_tp4/circle_perimeter.s
@@ -5,10 +5,27 @@
 .globl _ZNK7CCircle12PerimeterAsmEv
 
 _ZNK7CCircle12PerimeterAsmEv:
-        push %ebp      /* save old base pointer */
-        mov %esp, %ebp /* set ebp to current esp */
+        push %ebp               /* save old base pointer */
+        mov %esp, %ebp          /* set ebp to current esp */
 
-        mov 8(%ebp), %ebx       # bouge objet Ctriangle dans ebx
-        
-        leave          /* restore ebp and esp */
-        ret            /* return to the caller */
+	
+        movl 8(%ebp), %ebx       # %ebx = CCircle
+
+		# Ajouter mRadius sur la pile de float
+		fld 4(%ebx)				# st[0] =  mRadius
+
+		# Ajouter PI sur la pile de float
+		fldpi					# st[0] = PI | st[1] = mRadius
+		
+		# PI * mRadius
+		fmulp					# st[0] = st[1] = PI * mRadius
+
+		# Ajouter factor sur la pile de float
+		fld factor				# st[0] = factor | st[1] = PI * mRadius
+
+		# 2.0 * PI * mRadius
+		fmulp					# st[0] = st[1] = PI * mRadius * factor
+
+
+        leave					/* restore ebp and esp */
+        ret						/* return to the caller */
