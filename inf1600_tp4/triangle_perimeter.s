5d6f69da4ab52522c5e75600895d8a88d0e65f3c
Alex <alexandre.touchette@outlook.com>
2019-03-12 10:45:13 -0400

    Modification de triangle_height.s et de triangle_perimeter.s

--- inf1600_tp4/triangle_perimeter.s
+++ inf1600_tp4/triangle_perimeter.s
@@ -5,13 +5,13 @@ _ZNK9CTriangle12PerimeterAsmEv:
         mov %esp, %ebp /* set ebp to current esp */
         
         # Met le pointeur this dans %eax
-        mov 8(%ebp), %eax        # %eax = this
+        movl 8(%ebp), %eax        # %eax = this
         
         # Met le premier côté dans %ebx
-        mov 4(%eax), %ebx       # %ebx = mSides[0]
+        movl 4(%eax), %ebx       # %ebx = mSides[0]
 
         # Met le deuxième côté dans %ecx
-        mov 4(%eax), %ecx       # %ecx = mSides[1]
+        movl 4(%eax), %ecx       # %ecx = mSides[1]
 
         # Met %ebx au dessus de la pile
         push %ebx               # st[0] = %ebx
@@ -23,7 +23,7 @@ _ZNK9CTriangle12PerimeterAsmEv:
         faddp                   #st[0] = st[1] = %ebx + %ecx = mSides[1] + mSides[0]
 
         # Met le troisième côté dans %edx
-        mov 4(%eax), %edx       # %edx = mSides[2]
+        movl 4(%eax), %edx       # %edx = mSides[2]
 
         # Additione le troisième côté au deux premier
         faddp                   # st[0] = st[1] = mSides[2] + (mSides)[1] + mSides[0])
